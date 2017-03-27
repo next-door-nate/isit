979329c63f19cc133257d6f4ac605701d7aeef67
Nate <Nate@bold-admins-MBP.hitronhub.home>
2017-03-26 22:08:39 -0500

    creating gh-pages for the new domain

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function(window){
+	"use strict";
+	function goodnight() {
+		var Goodnight = {};
+		var hours = new Date().getHours();
+
+		Goodnight.AM = 20; // 6PM
+		Goodnight.PM = 6;    // 6AM
+
+		Goodnight.css = function(path) {
+			if (hours >= Goodnight.PM || hours < Goodnight.AM) {
+				var dark = document.createElement("link");
+				dark.rel = "stylesheet";
+				dark.href = path || "css/420.css";
+				document.getElementsByTagName("head")[0].appendChild(dark);
+			}
+		};
+
+		return Goodnight;
+	}
+
+	window.Goodnight = goodnight();
+}(window));
